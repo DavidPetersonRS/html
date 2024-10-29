54f9b21e19ac3247ea30b3a8fc376942068b87f5
David Peterson <david.peterson@rackspace.com>
2024-10-29 14:30:18 -0500

    adding rockwell site

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Get the App Service name from the environment variable
-$appServiceName = getenv('WEBSITE_SITE_NAME');
-?>
-<html>
-  <body>
-    <h1>Hello from <?php echo htmlspecialchars($appServiceName); ?></h1>
-  </body>
-</html>
